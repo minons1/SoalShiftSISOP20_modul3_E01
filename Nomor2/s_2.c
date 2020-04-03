e3ee135fdec0f6642722daea554fac6e6c22dcb3
Salim <55347970+minons1@users.noreply.github.com>
2020-04-03 11:22:10 +0700

    Update s_2.c

--- Nomor2/s_2.c
+++ Nomor2/s_2.c
@@ -56,17 +56,20 @@ int main(){
         memset(command,0,sizeof(command));
         memset(username,0,sizeof(username));
         memset(password,0,sizeof(password));
-
+        
+        //terima inputan command
         read( new_socket , command, 64);
         
-
+        //terima input username dan password
         read ( new_socket, username, 64);
         read ( new_socket, password, 64);
         username[strlen(username)]='\n';
         password[strlen(password)]='\n';
-
+        
+        //ketika command login
         if(tolower(command[0])=='l'){
             flag=0;
+            //iterasi kedalam file akun.txt untuk multi autentikasi
             while(fgets(iter,64,file)){
                 if(strcmp(iter,username)==0){
                     fgets(iter,64,file);
@@ -86,19 +89,23 @@ int main(){
             }
             
         }
+        //ketika command register
         else if(command[0]=='r'){
             i=0;
+            //iterasi untuk mencetak username dan password yang telah ada
             while(fgets(iter,64,file)){
                 printf("%d. username : %s",++i,iter);
                 fgets(iter,64,file);
                 printf("password : %s",iter);
             }
+            //menuliskan username dan password baru
             fwrite(username,1,strlen(username),file);
             fwrite(password,1,strlen(password),file);
             
             
             send( new_socket, "register success",strlen("register success"),0);
         }
+        //command untuk keluar dari game
         else{
             break;
         }
