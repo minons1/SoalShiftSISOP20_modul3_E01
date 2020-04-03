6de86f46355b094174309b96d2b44b955e9e5ff0
Salim <55347970+minons1@users.noreply.github.com>
2020-04-03 11:34:46 +0700

    Update 4c.c

--- Nomor4/4c.c
+++ Nomor4/4c.c
@@ -9,14 +9,18 @@
 int main(){
     int fd[2];
     pipe(fd);
+    //menjalankan command ls
     if(fork()==0){
+        //output dari ls dimasukkan ke pipe
         dup2(fd[1], STDOUT_FILENO);
         close(fd[0]);
         close(fd[1]);
 
         execlp("ls","ls",(char*)NULL);
     }
+    //menjalankan command wc -l
     if(fork()==0){
+        //mengambil input untuk command wc -l dari pipe
         dup2(fd[0],STDIN_FILENO);
         close(fd[1]);
         close(fd[0]);
